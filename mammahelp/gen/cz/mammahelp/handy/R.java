14d830db6379724442ae95b0221cc8b49d8bd4e5
hPa <petr@vranik.name>
2014-09-11 16:28:42 +0200

    gradle podpora pro eclipse

--- mammahelp/gen/cz/mammahelp/handy/R.java
+++ mammahelp/gen/cz/mammahelp/handy/R.java
@@ -1270,12 +1270,13 @@ containing a value of this type.
         public static final int category_listing=0x7f03001c;
         public static final int errorview=0x7f03001d;
         public static final int fragment_article_detail_view=0x7f03001e;
-        public static final int fragment_main=0x7f03001f;
-        public static final int fragment_navigation_drawer=0x7f030020;
-        public static final int slider_preference=0x7f030021;
-        public static final int spinner_list=0x7f030022;
-        public static final int spinner_view=0x7f030023;
-        public static final int support_simple_spinner_dropdown_item=0x7f030024;
+        public static final int fragment_home=0x7f03001f;
+        public static final int fragment_main=0x7f030020;
+        public static final int fragment_navigation_drawer=0x7f030021;
+        public static final int slider_preference=0x7f030022;
+        public static final int spinner_list=0x7f030023;
+        public static final int spinner_view=0x7f030024;
+        public static final int support_simple_spinner_dropdown_item=0x7f030025;
     }
     public static final class menu {
         public static final int category_list=0x7f0e0000;
