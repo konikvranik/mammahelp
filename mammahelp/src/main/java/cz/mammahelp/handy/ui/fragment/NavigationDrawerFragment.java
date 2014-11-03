4a847d585495348260c047fa339d6f8c94e2dbd7
hPa <petr@vranik.name>
2014-11-03 11:58:43 +0100

    po startu vyber novinek

--- mammahelp/src/main/java/cz/mammahelp/handy/ui/fragment/NavigationDrawerFragment.java
+++ mammahelp/src/main/java/cz/mammahelp/handy/ui/fragment/NavigationDrawerFragment.java
@@ -91,7 +91,7 @@ public class NavigationDrawerFragment extends Fragment {
 		}
 
 		// Select either the default item (0) or the last selected item.
-		// selectItem(mCurrentSelectedPosition);
+		selectItem(1, 0);
 	}
 
 	@Override
