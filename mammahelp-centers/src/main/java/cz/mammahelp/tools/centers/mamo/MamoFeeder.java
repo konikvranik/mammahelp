9ee38c3c3419d25d82258ccaafd39ee2189d48ba
hPa <petr@vranik.name>
2014-11-05 12:32:15 +0100

    timeout check

--- mammahelp-centers/src/main/java/cz/mammahelp/tools/centers/mamo/MamoFeeder.java
+++ mammahelp-centers/src/main/java/cz/mammahelp/tools/centers/mamo/MamoFeeder.java
@@ -5,14 +5,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 import cz.mammahelp.tools.centers.GenericFeeder;
 
 public class MamoFeeder extends GenericFeeder {
 
 	private static final String URL = "http://www.mamo.cz/index.php?pg=mamograficky-screening--centra--seznam";
-	
-	
 
 	@Override
 	protected String getTemplateLocation() throws IOException {
@@ -21,16 +20,18 @@ public class MamoFeeder extends GenericFeeder {
 
 	@Override
 	public java.net.URL getUrl() throws MalformedURLException {
+		try {
+		return new URL(URL);
+		} catch (IOException e) {
 		return new File("tmp/mamograficky-screening--centra--seznam.html").toURI().toURL();
-		// return new URL(URL);
+		}
 	}
 
 	@Override
 	protected InputStream getTemplateStream() throws FileNotFoundException {
 		return getClass().getResourceAsStream("/templates/mamo_centra.xsl");
 	}
-	
-	
+
 	@Override
 	protected String getEncoding() {
 		return "iso-8859-2";
