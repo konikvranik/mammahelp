5709852e7beb54c72525b5f5a08484713e6ae1db
hPa <petr@vranik.name>
2014-11-13 13:23:07 +0100

    no fake location

--- mammahelp-android/src/main/java/cz/mammahelp/handy/AndroidUtils.java
+++ mammahelp-android/src/main/java/cz/mammahelp/handy/AndroidUtils.java
@@ -162,11 +162,10 @@ public class AndroidUtils {
 		if (loc == null)
 			loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 		log.debug("Last known location: " + loc);
-		if (loc == null && false) {
-			loc = new Location("fake");
-			loc.setLatitude(50.083468);
-			loc.setLongitude(14.479357);
-		}
+		/*
+		 * if (loc == null ) { loc = new Location("fake");
+		 * loc.setLatitude(50.083468); loc.setLongitude(14.479357); }
+		 */
 		return loc;
 
 	}
